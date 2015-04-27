1a29592fbeea99f4f533d118efb98b296f196316
c0ming <c0ming@qq.com>
2015-04-27 15:05:12 +0800

    Initial Commit

---
+++ HuggingAndCompressionResistanceDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HuggingAndCompressionResistanceDemo
+//
+//  Created by c0ming on 4/27/15.
+//  Copyright (c) 2015 c0ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HuggingAndCompressionResistanceDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HuggingAndCompressionResistanceDemo
+//
+//  Created by c0ming on 4/27/15.
+//  Copyright (c) 2015 c0ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
